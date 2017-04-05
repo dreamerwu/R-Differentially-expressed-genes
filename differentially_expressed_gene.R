f12482bc583f7602b7d15068455255e5474665cc
dreamerwu <haowbdreamer@gmail.com>
2017-04-05 12:45:50 -0400

    Update differentially_expressed_gene.R

--- differentially_expressed_gene.R
+++ differentially_expressed_gene.R
@@ -4,11 +4,13 @@ library("affy") #use affy package to do background correcting & Normalizing & Ca
 library("limma") #use limma package to find differentially expressed genes
 data=ReadAffy(celfile.path="~/../../..") #read .CEL files into data
 eset=rma(data) #use rma to normalize data
+matrix=as.matrix(eset) #convert S4 file to matrix
+write.csv(matrix,"~/../../xxx.csv") #write expression data
 design=model.matrix(~ 0+factor(c(1,1,1,1,0,0,0,0)))
 colnames(design)=c("group1","group2")
 fit=lmFit(eset,design)
 contrast.matrix=makeContrasts(group2-group1,levels=design)
-fit2=contrasts.fit(fix,contrast.matrix)
+fit2=contrasts.fit(fit,contrast.matrix)
 fit2=eBayes(fit2)
 result=topTable(fit2,number=60000,adjust.method="BH",p.value=1,lfc=0)
 

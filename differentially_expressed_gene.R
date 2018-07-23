eb17b12a647cb98e1d1e75dc66b8ab0acd3a0dea
dreamerwu <haowbdreamer@gmail.com>
2018-07-22 20:42:18 -0400

    Update differentially_expressed_gene.R

--- differentially_expressed_gene.R
+++ differentially_expressed_gene.R
@@ -14,4 +14,25 @@ fit2=contrasts.fit(fit,contrast.matrix)
 fit2=eBayes(fit2)
 result=topTable(fit2,number=60000,adjust.method="BH",p.value=1,lfc=0)
 
+＃usage: gene expression is known, calculating P value;
+library("limma")  # use limma package to identify differentially expressed genes
+data=read.delim("~/Desktop/TJ_DEG.txt",head=T,sep="\t") # reading gene expression file, first
+data2=data.matrix(data)
+data3=data2[,2:ncol(data2)]
+design=model.matrix(~ 0+factor(c(1,1,1,0,0,0)))
+colnames(design)=c("untreat","treat")
+fit=lmFit(data3,design)
+contrast.matrix=makeContrasts(treat-untreat,levels=design)
+fit2=contrasts.fit(fit,contrast.matrix)
+fit3=eBayes(fit2)
+result=topTable(fit3,number=60000,adjust.method="BH",p.value=1,lfc=0)
+write.csv(result,"~/Desktop/result.csv")
+
+
+
+
+
+
+
+
 
